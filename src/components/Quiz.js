772d5c1e8de2545f29830c374a15efc3e2cfae2d
Johanna Nilsson <nijodotcom@gmail.com>
2020-03-28 12:02:27 +0100

    form works

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,38 +1,82 @@
 import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
+import { useForm } from 'react-hook-form';
 
-import RenderQuiz from './RenderQuiz';
+// import RenderQuiz from './RenderQuiz';
 
-// import Container from 'react-bootstrap/Container';
-// import Col from 'react-bootstrap/Col';
-// import Row from 'react-bootstrap/Row';
-// import Card from 'react-bootstrap/Card';
-// import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container'; 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 export default function Quiz({ questions }) {
   const [submitAnswer, setSubmitAnswer] = useState(false);
-  const [answers, setAnswers] = useState();
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log('DATA IS ----> ', data);
+  console.log('RENDERING');
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(e.target.value);
-    setSubmitAnswer(true);
-  }
-
-  function handleInput(answer) {
-    console.log(answer);
-  }
   return (
     <>
       <Helmet>
         <title>Quiz</title>
       </Helmet>
-      <RenderQuiz
-        questions={questions}
-        submitAnswer={submitAnswer}
-        handleSubmit={handleSubmit}
-        handleInput={handleInput}
-      />
+      <Container fluid>
+        <Row>
+          <Col>
+            <h1>Quiz</h1>
+          </Col>
+        </Row>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {questions.map((question, idx) => {
+            const number = (idx += 1);
+
+            let options = question.incorrect_answers.concat(question.correct_answer);
+            // console.log('options concated  ',options);
+            // console.log(question.correct_answer);
+
+            const mixedOptions = options.sort(() => Math.random() - 0.5);
+            // console.log(mixedOptions);
+
+            return (
+              <Row key={number}>
+                <Col>
+                  <Card className='text-center'>
+                    <Card.Header>Question number {number}</Card.Header>
+                    <Card.Body>
+                      <Card.Title>{question.question}</Card.Title>
+
+                      {mixedOptions.map((option, index) => {
+                        const uniqueKey = `${number}${index}`;
+
+                        return (
+                          <React.Fragment key={uniqueKey}>
+                            <input
+                              name={question.question}
+                              type='radio'
+                              id={`${number}-${option}`}
+                              value={option}
+                              ref={register({ required: true })}
+                              // checked={}
+                              // onChange={() => handleInput(`${number}`, `${option}`)}
+                            />
+                            <label htmlFor={`${number}-${option}`}>
+                              {option}
+                            </label>
+                          </React.Fragment>
+                        );
+                        // }
+                      })}
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            );
+          })}
+          <button variant='info' type='submit'>
+            {submitAnswer ? 'Checking...' : 'Submit'}
+          </button>
+        </form>
+      </Container>
     </>
   );
 }
--- src/components/RenderQuiz.js
+++ src/components/RenderQuiz.js
@@ -1,87 +1,63 @@
 import React from 'react';
 
-import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 
-export default function RenderQuiz({questions, handleSubmit, handleInput},submitAnswer) {
-    console.log('input ', handleInput);
-    console.log('sub', handleSubmit);
-    console.log('sub ans ',  submitAnswer,);
+export default function RenderQuiz({
+  questions,
+  submitAnswer,
+  onSubmit
+},
+answers) {
+  // console.log('answers ', answers);
+  console.log('when', questions);
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <h1>Quiz</h1>
-        </Col>
-      </Row>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        {questions.map((question, idx) => {
-          const number = (idx += 1);
+    <form onSubmit={onSubmit(onSubmit)}>
+      {questions.map((question, idx) => {
+        const number = (idx += 1);
 
-          let options = question.incorrect_answers;
-          options.push(question.correct_answer);
+        let options = question.incorrect_answers;
+        options.push(question.correct_answer);
 
-          const mixedOptions = options.sort(() => Math.random() - 0.5);
+        const mixedOptions = options.sort(() => Math.random() - 0.5);
 
-          return (
-            <Row key={number}>
-              <Col>
-                <Card className='text-center'>
-                  <Card.Header>Question number {number}</Card.Header>
-                  <Card.Body>
-                    <Card.Title>{question.question}</Card.Title>
+        return (
+          <Row key={number}>
+            <Col>
+              <Card className='text-center'>
+                <Card.Header>Question number {number}</Card.Header>
+                <Card.Body>
+                  <Card.Title>{question.question}</Card.Title>
 
-                    {mixedOptions.map((option, index) => {
-                      const uniqueKey = `${number}${index}`;
+                  {mixedOptions.map((option, index) => {
+                    const uniqueKey = `${number}${index}`;
 
-                      if (index === 0) {
-                        return (
-                          <React.Fragment key={uniqueKey}>
-                            <input
-                              // key={uniqueKey}
-                              type='radio'
-                              id={`${number}-${option}`}
-                              value={option}
-                              name={question.question}
-                              defaultChecked
-                              onChange={(e) => handleInput}
-                            />
-                            <label htmlFor={`${number}-${option}`}>
-                              {option}
-                            </label>
-                          </React.Fragment>
-                        );
-                      } else {
-                        return (
-                          <React.Fragment key={uniqueKey}>
-                            <input
-                              key={uniqueKey + '0'}
-                              type='radio'
-                              id={`${number}-${option}`}
-                              value={option}
-                              name={question.question}
-                              onChange={(e) => console.log(e.target.checked, e.target.value)}
-                            />
-                            <label htmlFor={`${number}-${option}`}>
-                              {option}
-                            </label>
-                          </React.Fragment>
-                        );
-                      }
-                    })}
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          );
-        })}
-        <Button variant='info'>
-          {submitAnswer ? 'Checking...' : 'Submit'}
-        </Button>
-      </form>
-    </Container>
+                    return (
+                      <React.Fragment key={uniqueKey}>
+                        <input
+                          name={question.question}
+                          type='radio'
+                          id={`${number}-${option}`}
+                          value={option}
+                          ref={answers({ required: true })}
+                          // checked={}
+                          // onChange={() => handleInput(`${number}`, `${option}`)}
+                        />
+                        <label htmlFor={`${number}-${option}`}>{option}</label>
+                      </React.Fragment>
+                    );
+                    // }
+                  })}
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        );
+      })}
+      <button variant='info' type='submit'>
+        {submitAnswer ? 'Checking...' : 'Submit'}
+      </button>
+    </form>
   );
 }
