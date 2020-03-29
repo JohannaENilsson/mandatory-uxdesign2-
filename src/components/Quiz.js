783a9c192d74af0bc974d480f8adbe904ece042a
Johanna Nilsson <nijodotcom@gmail.com>
2020-03-29 09:14:13 +0200

    saves user answers

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,15 +4,20 @@ import { useForm } from 'react-hook-form';
 
 // import RenderQuiz from './RenderQuiz';
 
-import Container from 'react-bootstrap/Container'; 
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
 export default function Quiz({ questions }) {
   const [submitAnswer, setSubmitAnswer] = useState(false);
+  const [submitedAnswers, setsubmitedAnswers] = useState({});
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => console.log('DATA IS ----> ', data);
+  const onSubmit = data => {
+    console.log('DATA IS ----> ', data);
+    setsubmitedAnswers(data);
+    setSubmitAnswer(true);
+  };
   console.log('RENDERING');
 
   return (
@@ -26,50 +31,57 @@ export default function Quiz({ questions }) {
             <h1>Quiz</h1>
           </Col>
         </Row>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)}
+        aria-label="Quiz"
+        >
           {questions.map((question, idx) => {
             const number = (idx += 1);
 
-            let options = question.incorrect_answers.concat(question.correct_answer);
-            // console.log('options concated  ',options);
-            // console.log(question.correct_answer);
+            let options = question.incorrect_answers.concat(
+              question.correct_answer
+            );
 
             const mixedOptions = options.sort(() => Math.random() - 0.5);
-            // console.log(mixedOptions);
 
             return (
-              <Row key={number}>
-                <Col>
-                  <Card className='text-center'>
-                    <Card.Header>Question number {number}</Card.Header>
-                    <Card.Body>
-                      <Card.Title>{question.question}</Card.Title>
+              <fieldset
+                key={number}
+                aria-labelledby={`question_head-${number}`}
+                >
+              
+                <legend id={`question_head-${number}`}>
+                  {question.question}
+                </legend>
+                {/* <Row> */}
+                {/* <Col> */}
+                {/* <Card className='text-center'> */}
+                {/* <Card.Header><h2>Question number {number}</h2></Card.Header> */}
+                {/* <Card.Body> */}
+                {/* <Card.Title><h2>{question.question}</h2></Card.Title> */}
 
-                      {mixedOptions.map((option, index) => {
-                        const uniqueKey = `${number}${index}`;
+                {mixedOptions.map((option, index) => {
+                  const uniqueKey = `${number}${index}`;
 
-                        return (
-                          <React.Fragment key={uniqueKey}>
-                            <input
-                              name={question.question}
-                              type='radio'
-                              id={`${number}-${option}`}
-                              value={option}
-                              ref={register({ required: true })}
-                              // checked={}
-                              // onChange={() => handleInput(`${number}`, `${option}`)}
-                            />
-                            <label htmlFor={`${number}-${option}`}>
-                              {option}
-                            </label>
-                          </React.Fragment>
-                        );
-                        // }
-                      })}
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
+                  return (
+                    <React.Fragment key={uniqueKey}>
+                      <label htmlFor={`${number}-${option}`}>{option}</label>
+
+                      <input
+                        name={number}
+                        type='radio'
+                        id={`${number}-${option}`}
+                        value={option}
+                        ref={register({ required: true })}
+                      />
+                    </React.Fragment>
+                  );
+                  // }
+                })}
+                {/* </Card.Body> */}
+                {/* </Card> */}
+                {/* </Col> */}
+                {/* </Row> */}
+              </fieldset>
             );
           })}
           <button variant='info' type='submit'>
